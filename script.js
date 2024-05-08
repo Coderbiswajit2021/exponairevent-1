81caddb06d2d877be90ea4d4a2de8bc0f1c10a16
Coderbiswajit2021 <sahoobiswajit969@gmail.com>
2024-05-08 11:55:17 +0530

    update-1

---
+++ script.js
@@ -0,0 +1,89 @@
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+let menu = document.querySelector("#menuBars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+
+
+// review section 
+
+var swiper = new Swiper(".ReviewSlide", {
+    slidesPerView: 1,
+    grabCuror: true,
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+
+    },
+});
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+})
+
+
+
+sr.reveal(`.scroll-top, .footer-row`)
+sr.reveal(`.about-img `, { delay: 800, scale: .5 })
+
+const srt = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+})
+
+srt.reveal(`.box-1`)
+// sr.reveal(`.box-1 `,{origin : 'left'})
+// sr.reveal(`.box-2 `,{origin : 'right'})
+
+// ScrollReveal().reveal('.content');
+
+
+function downloadPDF() {
+    var pdfUrl = "E-Brochure/COMPANY PROFILE (1).pdf";
+
+    var link = document.createElement('a');
+    link.href = pdfUrl;
+
+    link.setAttribute('download', 'filename.pdf');
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
+
+document.getElementById('downloadButton').addEventListener('click', downloadPDF);
